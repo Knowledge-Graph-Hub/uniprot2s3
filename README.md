25054699458807e30063cca2ae5eb266660fdaee
bsantan <70932395+bsantan@users.noreply.github.com>
2024-02-16 16:34:58 -0700

    Second commit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Uniprot download to s3.
 
+To run, save the `ncbitaxon_removed_subset.json` to the `data/raw` directory and run `make all`.
+
 # Acknowledgements
 
 This [cookiecutter](https://cookiecutter.readthedocs.io/en/stable/README.html) project was developed from the [monarch-project-template](https://github.com/monarch-initiative/monarch-project-template) template and will be kept up-to-date using [cruft](https://cruft.github.io/cruft/).
---
+++ src/uniprot2s3/constants.py
@@ -0,0 +1,25 @@
+"""Constants for robot_utilities."""
+
+from pathlib import Path
+
+RAW_DATA_DIR = Path(__file__).parents[2] / "data" / "raw"
+
+# NCBITaxon
+NCBITAXON_PREFIX = "NCBITaxon:"
+
+# ROBOT
+ROBOT_REMOVED_SUFFIX = "_removed_subset"
+EXCLUSION_TERMS_FILE = "exclusion_branches.tsv"
+
+# Uniprot
+UNIPROT_BASE_URL = "https://rest.uniprot.org/uniprotkb/"
+UNIPROT_FIELDS = ["organism_id", "id", "accession", "protein_name", "ec", "ft_binding"]
+UNIPROT_KEYWORDS = ["Reference+proteome"]  # Not useful
+UNIPROT_DESIRED_FORMAT = "tsv"
+UNIPROT_SIZE = 500
+ORGANISM_ID_MIXED_CASE = "Organism_ID"
+TAXONOMY_ID_UNIPROT_PREFIX = "taxonomy_id:"
+UNIPROT_REVIEWED_FLAG = "reviewed:true+"  # Not useful
+UNIPROT_REFERENCE_PROTEOMES_URL = "https://rest.uniprot.org/proteomes/"
+UNIPROT_REFERENCE_PROTEOMES_FIELDS = ["upid", "organism_id"]
+PROTEOMES_FILENAME = "Proteomes"
